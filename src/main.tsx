b409e29c4f707b356c59710cfc6b4d8194494d25
Mohamed Farag <mf1456@fayoum.edu.eg>
2023-03-26 02:26:29 +0200

    done

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,8 @@ import './index.css'
 import App from './App'
 
 // set axios base url
-axios.defaults.baseURL = 'http://localhost:8000/api/v1'
+axios.defaults.baseURL = '/api/v1'
+// axios.defaults.baseURL = 'http://localhost:8000/api/v1'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
